7dc2f32c1e40b12c845e2d7d6e3a6557ca8d5173
Henrik <hgg1998@gmail.com>
2021-04-02 18:12:23 +0200

    Train autoencoder.

--- autoencoder.py
+++ autoencoder.py
@@ -138,6 +138,7 @@ def autotest():
         x_train = x_train.astype('float32') / 255.0
         x_test = x_test.astype('float32') / 255.0
         y_train = to_categorical(y_train, 10)
+        print(x_train.shape)
         #x_train = x_train.reshape(450000, x_train.shape[1], x_train.shape[2], 3)
 
         input_shape = Input(shape=(32, 32, 3))
@@ -149,18 +150,27 @@ def autotest():
         layers = Conv2D(filters=64, kernel_size=(3, 3), activation='relu')(layers)
         layers = MaxPooling2D((2, 2))(layers)
         layers = Flatten()(layers)
-        layers = Dense(128, activation='relu')(layers)
-        layers = Dense(10, activation='softmax')(layers)
+        layers = Dense(432, activation='relu')(layers)
+        layers = Reshape((12, 12, 3))(layers)
+        layers = Conv2DTranspose(filters=1, kernel_size=(15, 15), activation='relu')(layers)
+        layers = Conv2DTranspose(filters=1, kernel_size=(15, 15), activation='relu')(layers)
+        layers = MaxPooling2D((2, 2))(layers)
+        layers = Conv2DTranspose(filters=3, kernel_size=(20, 20), activation='relu')(layers)
+        layers = Conv2DTranspose(filters=3, kernel_size=(26, 26), activation='relu')(layers)
+        layers = MaxPooling2D((2, 2))(layers)
+        layers = Reshape((32, 32, 3))(layers)
+
+        #layers = Dense(10, activation='softmax')(layers)
         #layers = Dense(10, activation='relu')(layers)
         #layers = Dense(625, activation='relu')(layers)
-
         #layers = Reshape((25, 25, 1))(layers)
         #layers = Conv2DTranspose(filters=1, kernel_size=(8, 8), activation='sigmoid')(layers)
 
         opt = SGD(lr=0.1, momentum=0.9)
         autoencoder = Model(input_shape, layers)
         autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-        autoencoder.fit(x_train, y_train, epochs=4)
+        autoencoder.fit(x_train, x_train, epochs=1)
+        #autoencoder.fit(x_train, y_train, epochs=1)
         #plot_autoencoder_outputs(autoencoder, 5, (image_size, image_size), x_train)
         quit()
 
