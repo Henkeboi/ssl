cadf74c9ef647375eceedf97c6c919b8efa22645
Henrik <hgg1998@gmail.com>
2021-04-06 17:57:53 +0200

    Clean in code.

--- autoencoder.py
+++ autoencoder.py
@@ -38,50 +38,34 @@ def plot_autoencoder_outputs(autoencoder, n, dims, x_test):
             ax.set_title('Reconstructed Images')
         plt.show()
 
-class Autoencoder:
-    def __init__(self, input_shape, latent_size, encoder_layers, decoder_layers):
-        layers = Input(shape=input_shape)
-        input_shape = layers
-        for i in range(len(encoder_layers)):
-            layers = Dense(encoder_layers[i], activation='relu')(layers)
-
-        latent_layer = Dense(latent_size, activation='relu')(layers) # Latent layer
-
-        for i in range(len(decoder_layers)):
-            if i == len(decoder_layers) - 1:
-                layers = Dense(decoder_layers[i], activation='sigmoid')(layers)
-            elif i == 0:
-                layers = Dense(decoder_layers[i], activation='relu')(latent_layer)
-            else:
-                layers = Dense(decoder_layers[i], activation='relu')(layers)
-
-        self.autoencoder = Model(input_shape, layers)
-        self.autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-
-        classifier_layer = Dense(10, activation='sigmoid')(latent_layer) 
-        self.classifier = Model(input_shape, classifier_layer)
-        self.classifier.compile(optimizer='adam', loss='binary_crossentropy')
-
-    def train_autoencoder(self, x_train):
-        self.autoencoder.fit(x_train, x_train, epochs=1, batch_size=100)
-
-    def train_classifier(self, x_train, y_train):
-        self.classifier.fit(x_train, y_train, epochs=1, batch_size=100)
-
-    def evaluate(self, x_test, y_test):
-        return self.classifier.evaluate(x_test, y_test)
-
-
 class Encoder:
-    def __init__(self):
-        image_size = 28
-        input_shape = (image_size * image_size)
-
-        layers = Input(shape=input_shape)
-        self.input_layer = layers
-        layers = Dense(784, activation='relu')(layers)
-        layers = Dense(120, activation='relu')(layers)
-        self.latent_layer = Dense(1000, activation='relu')(layers) # Latent layer
+    def __init__(self, dataset):
+        self.dataset = dataset
+        if dataset == 'mnist':
+            image_size = 28
+            input_shape = (image_size * image_size)
+            layers = Input(shape=input_shape)
+            self.input_layer = layers
+            layers = Dense(784, activation='relu')(layers)
+            layers = Dense(120, activation='relu')(layers)
+            self.latent_layer = Dense(1000, activation='relu')(layers) # Latent layer
+        elif dataset == 'fashion_mnist':
+            image_size = 28
+            input_shape = (image_size * image_size)
+            layers = Input(shape=input_shape)
+            self.input_layer = layers
+            layers = Dense(784, activation='relu')(layers)
+            layers = Dense(120, activation='relu')(layers)
+            self.latent_layer = Dense(1000, activation='relu')(layers) # Latent layer
+        elif dataset == 'cifer10':
+            input_shape = Input(shape=(32, 32, 3))
+            layers = input_shape
+            self.input_layer = layers
+            layers = Conv2D(filters=32, kernel_size=3, strides=1, padding='same', activation='relu')(layers)
+            layers = BatchNormalization()(layers)
+            layers = Conv2D(filters=32, kernel_size=3, strides=2, padding='same', activation='relu')(layers)
+            layers = BatchNormalization()(layers)
+            self.latent_layer = Dense(600, activation='relu')(layers)
     
     def is_trainable(trainable):
         for layer in self.encoder.layers:
@@ -97,202 +81,114 @@ class Encoder:
     def get_encoder(self):
         return self.encoder
 
-class Auto:
-    def __init__(self, encoder):
-        latent_layer = encoder.get_latent_layer()
-        layers = Dense(120, activation='relu')(latent_layer)
-        layers = Dense(784, activation='sigmoid')(layers)
-        self.autoencoder = Model(encoder.get_input_layer(), layers)
-        self.autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+class Autoencoder:
+    def __init__(self, encoder, do_training, store_parameters_after_training, model_name):
+        self.do_training = do_training
+        self.store_parameters_after_training = store_parameters_after_training
+        self.model_name = model_name
+        if encoder.dataset == "mnist":
+            latent_layer = encoder.get_latent_layer()
+            layers = Dense(120, activation='relu')(latent_layer)
+            layers = Dense(784, activation='sigmoid')(layers)
+            self.autoencoder = Model(encoder.get_input_layer(), layers)
+            self.autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        elif encoder.dataset == 'fashion_mnist':
+            latent_layer = encoder.get_latent_layer()
+            layers = Dense(120, activation='relu')(latent_layer)
+            layers = Dense(784, activation='sigmoid')(layers)
+            self.autoencoder = Model(encoder.get_input_layer(), layers)
+            self.autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        elif encoder.dataset == 'cifer10':
+            latent_layer = encoder.get_latent_layer()
+            layers = UpSampling2D()(latent_layer)
+            layers = Conv2D(32, kernel_size=3, strides=1, padding='same', activation='relu')(layers)
+            layers = BatchNormalization()(layers)
+            layers = Conv2D(3,  kernel_size=1, strides=1, padding='same', activation='sigmoid')(layers)
+            self.autoencoder = Model(encoder.get_input_layer(), layers)
+            self.autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
     def get_autoencoder(self):
         return self.encoder
 
     def train(self, x_train):
-        self.autoencoder.fit(x_train, x_train, epochs=1, batch_size=10)
+        if self.do_training == True:
+            self.autoencoder.fit(x_train, x_train, epochs=1, batch_size=100)
+            if self.store_parameters_after_training == True:
+                utility.store_model(self.autoencoder, self.model_name)
+        else:
+            self.autoencoder = utility.load_model(self.model_name)
+            self.autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
 class Classifier:
-    def __init__(self, encoder):
-        input_layer = encoder.get_input_layer()
-        latent_layer = encoder.get_latent_layer()
-        classifier_layer = Dense(10, activation='sigmoid')(latent_layer) 
-        self.classifier = Model(input_layer, classifier_layer)
-        self.classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    def __init__(self, encoder, do_training, store_parameters_after_training, model_name):
+        self.do_training = do_training
+        self.store_parameters_after_training = store_parameters_after_training
+        self.model_name = model_name
+        if encoder.dataset == 'mnist':
+            input_layer = encoder.get_input_layer()
+            latent_layer = encoder.get_latent_layer()
+            classifier_layer = Dense(10, activation='sigmoid')(latent_layer) 
+            self.classifier = Model(input_layer, classifier_layer)
+            self.classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        elif encoder.dataset == 'fashion_mnist':
+            input_layer = encoder.get_input_layer()
+            latent_layer = encoder.get_latent_layer()
+            classifier_layer = Dense(10, activation='sigmoid')(latent_layer) 
+            self.classifier = Model(input_layer, classifier_layer)
+            self.classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+        elif encoder.dataset == 'cifer10':
+            latent_layer = encoder.get_latent_layer()
+            classifier_layer = Flatten()(latent_layer) 
+            classifier_layer = Dense(10, activation='sigmoid')(classifier_layer) 
+            input_layer = encoder.get_input_layer()
+            self.classifier = Model(input_layer, classifier_layer)
+            self.classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
     def train_classifier(self, x_train, y_train):
-        self.classifier.fit(x_train, y_train, epochs=1, batch_size=100)
+        if self.do_training == True:
+            self.classifier.fit(x_train, y_train, epochs=1, batch_size=100)
+            if self.store_parameters_after_training == True:
+                utility.store_model(self.classifier, self.model_name)
+        else:
+            self.classifier = utility.load_model(self.model_name)
+            self.classifier.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
 
     def evaluate(self, x_test, y_test):
-        print(self.classifier.evaluate(x_test, y_test, verbose=1))
+        return self.classifier.evaluate(x_test, y_test, verbose=1)
  
-
-
-def autotest():
-    dataset = "mnist"
-    if dataset == "mnist":
-        image_size = 28
-        (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
-        x_train = x_train.astype('float32') / 255.0
-        x_test = x_test.astype('float32') / 255.0
-        x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-        x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-        y_test = to_categorical(y_test, 10)
-        y_train = to_categorical(y_train, 10)
-
-        # Unlabled
-        x_train_D1 = x_train[0 : len(x_train) - len(x_train) // 20]
-        y_train_D1 = None
-        # Labled
-        x_train_D2 = x_train[len(x_train) - len(x_train) // 20 :]
-        y_train_D2 = y_train[len(y_train) - len(y_train) // 20 :]
-
-        encoder = Encoder()
-        autoencoder = Auto(encoder)
-        autoencoder.train(x_train) 
-
-        classifier = Classifier(encoder)
-        classifier.train_classifier(x_train, y_train)
-        classifier.evaluate(x_test, y_test)
-
-        encoder = Encoder()
-        classifier = Classifier(encoder)
-        classifier.train_classifier(x_train, y_train)
-        classifier.evaluate(x_test, y_test)
-
-
-        quit()
-       
-
-        latent_units_size = 1000
-        autoencoder = Autoencoder((image_size * image_size), latent_units_size, [784, 120], [120, 784])
-        autoencoder.train_autoencoder(x_train_D1)
-        autoencoder.train_autoencoder(x_train_D1)
-        autoencoder.train_classifier(x_train_D2, y_train_D2)
-
-        classifier = Autoencoder((image_size * image_size), latent_units_size, [784, 120], [120, 784])
-        classifier.train_classifier(x_train_D2, y_train_D2)
-
-        result1 = autoencoder.classifier.evaluate(x_test, y_test, verbose=0)
-        result2 = classifier.classifier.evaluate(x_test, y_test, verbose=0)
-        print("Loss autoencoder:")
-        print(result1)
-        print("Loss classifier")
-        print(result2)
-        quit()
-        #plot_autoencoder_outputs(autoencoder.autoencoder, 5, (image_size, image_size), x_train)
-    elif dataset == "fashion_mnist":
-        image_size = 28
-        latent_units_size = 100
-        (x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
-        x_train = x_train.astype('float32') / 255.0
-        x_test = x_test.astype('float32') / 255.0
-        x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
-        x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
-        y_test = to_categorical(y_test, 10)
-        y_train = to_categorical(y_train, 10)
-
-        # Unlabled
-        x_train_D1 = x_train[0 : len(x_train) - len(x_train) // 30]
-        y_train_D1 = None
-        # Labled
-        x_train_D2 = x_train[len(x_train) - len(x_train) // 30 :]
-        y_train_D2 = y_train[len(y_train) - len(y_train) // 30 :]
-    
-        autoencoder = Autoencoder((image_size * image_size), latent_units_size, [784, 120], [120, 784])
-        autoencoder.train_autoencoder(x_train_D1)
-        autoencoder.train_classifier(x_train_D2, y_train_D2)
-
-        classifier = Autoencoder((image_size * image_size), latent_units_size, [784, 120], [120, 784])
-        classifier.train_classifier(x_train_D2, y_train_D2)
-
-        result1 = autoencoder.evaluate(x_test, y_test)
-        result2 = classifier.evaluate(x_test, y_test)
-        print("Loss autoencoder: " + str(result1))
-        print("Loss classifier: " + str(result2))
-        
-        #plot_autoencoder_outputs(autoencoder.autoencoder, 5, (image_size, image_size), x_train)
-    else:
-        image_size = 32
-        latent_units_size = 20
-        (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
-        x_train = x_train.astype('float32') / 255.0
-        x_test = x_test.astype('float32') / 255.0
-        y_test = to_categorical(y_test, 10)
-        y_train = to_categorical(y_train, 10)
-
-        # Unlabled
-        x_train_D1 = x_train[0 : len(x_train) - len(x_train) // 30]
-        y_train_D1 = None
-        # Labled
-        x_train_D2 = x_train[len(x_train) - len(x_train) // 20 :]
-        y_train_D2 = y_train[len(y_train) - len(y_train) // 20 :]
-
-        input_shape = Input(shape=(32, 32, 3))
-        layers = input_shape
-        layers = Conv2D(filters=32, kernel_size=3, strides=1, padding='same', activation='relu')(layers)
-        layers = BatchNormalization()(layers)
-        layers = Conv2D(filters=32, kernel_size=3, strides=2, padding='same', activation='relu')(layers)
-        layers = BatchNormalization()(layers)
-        layers = Dense(400, activation='relu')(layers)
-        latent_layer = layers
-        layers = UpSampling2D()(layers)
-        layers = Conv2D(32, kernel_size=3, strides=1, padding='same', activation='relu')(layers)
-        layers = BatchNormalization()(layers)
-        layers = Conv2D(3,  kernel_size=1, strides=1, padding='same', activation='sigmoid')(layers)
-
-        autoencoder = Model(input_shape, layers)
-        autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-        #autoencoder.fit(x_train_D1, x_train_D1, epochs=1, batch_size=50)
-        #utility.store_model(autoencoder, 'autoencoder')
-        autoencoder = utility.load_model('autoencoder')
-        autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-        classifier_layer = Flatten()(latent_layer) 
-        classifier_layer = Dense(10, activation='sigmoid')(classifier_layer) 
-        classifier = Model(input_shape, classifier_layer)
-
-        #for layer in autoencoder.layers:
-        #    layer.trainable = False
-        opt = keras.optimizers.Adam(learning_rate=0.0005) 
-        classifier.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
-        classifier.fit(x_train_D2, y_train_D2, batch_size=5, epochs=1)
-        result1 = classifier.evaluate(x_test, y_test, verbose=0)
-        print("Autoencoder loss and accurarcy:")
-        print(result1)
-        #plot_autoencoder_outputs(autoencoder, 5, (image_size, image_size, 3), x_train)
-
 def main():
-    autotest()
-    (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
-    x_train = x_train.astype('float32') / 255.0
-    x_test = x_test.astype('float32') / 255.0
-    y_test = to_categorical(y_test, 10)
-    y_train = to_categorical(y_train, 10)
+    dataset = "cifer10"
+    (x_train, y_train), (x_test, y_test) = utility.get_dataset(dataset)
+    (x_train_D1, y_train_D1), (x_train_D2, y_train_D2) = utility.split_dataset(x_train, y_train)
 
-    # Unlabled
-    x_train_D1 = x_train[0 : len(x_train) - len(x_train) // 30]
-    y_train_D1 = None
-    # Labled
-    x_train_D2 = x_train[len(x_train) - len(x_train) // 20 :]
-    y_train_D2 = y_train[len(y_train) - len(y_train) // 20 :]
+    autoencoder_do_training = True
+    autoencoder_store_model = True
+    autoencoder_model_name = 'autoencoder' + str(dataset)
+    encoder = Encoder(dataset)
+    autoencoder = Autoencoder(encoder, autoencoder_do_training, autoencoder_store_model, autoencoder_model_name)
+    autoencoder.train(x_train) 
 
-    input_shape = Input(shape=(32, 32, 3))
-    layers = input_shape
-    layers = Conv2D(filters=32, kernel_size=3, strides=1, padding='same', activation='relu')(layers)
-    layers = BatchNormalization()(layers)
-    layers = Conv2D(filters=32, kernel_size=3, strides=2, padding='same', activation='relu')(layers)
-    layers = BatchNormalization()(layers)
-    layers = Dense(400, activation='relu')(layers)
-    layers = Flatten()(layers) 
-    layers = Dense(10, activation='sigmoid')(layers) 
-    classifier = Model(input_shape, layers)
-    opt = keras.optimizers.Adam(learning_rate=0.001)
-    classifier.compile(optimizer=opt, loss='binary_crossentropy', metrics=['accuracy'])
-    classifier.fit(x_train_D2, y_train_D2, batch_size=5, epochs=1)
+    classifier_do_training = True
+    classifier_store_model = True
+    classifer_model_name = 'auto_classifier' + str(dataset)
+    autoencoder_classifier = Classifier(encoder, classifier_do_training, classifier_store_model, classifer_model_name)
+    autoencoder_classifier.train_classifier(x_train, y_train)
+    loss, acc = autoencoder_classifier.evaluate(x_test, y_test)
+    print("Autoencoder classifier loss: " + str(loss) + ". Acc: " + str(acc))
 
-    result2 = classifier.evaluate(x_test, y_test, verbose=0)
-    print("Classifier loss and accurarcy:")
-    print(result2)
+    classifier_do_training = True
+    classifier_store_model = True
+    classifier_model_name = 'simple_classifier' + str(dataset)
+    encoder = Encoder(dataset)
+    simple_classifier = Classifier(encoder, classifier_do_training, classifier_store_model, classifier_model_name)
+    simple_classifier.train_classifier(x_train, y_train)
+    loss, acc = simple_classifier.evaluate(x_test, y_test)
+    print("Simple classifier loss: " + str(loss) + ". Acc: " + str(acc))
+
+    # plot_autoencoder_outputs(autoencoder.autoencoder, 5, (image_size, image_size), x_train)
+
+
 
 if __name__ == '__main__':
     main()
--- utility.py
+++ utility.py
@@ -1,5 +1,7 @@
+import numpy as np
+import keras
 from keras.models import model_from_json
-
+from keras.utils.np_utils import to_categorical
 
 def store_model(model, name):
     model_json = model.to_json()
@@ -15,3 +17,39 @@ def load_model(name):
     model.load_weights('data/' + name + '.h5')
     return model
 
+def get_dataset(dataset):
+    if dataset == "mnist":
+        image_size = 28
+        (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+        x_train = x_train.astype('float32') / 255.0
+        x_test = x_test.astype('float32') / 255.0
+        x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+        x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+        y_test = to_categorical(y_test, 10)
+        y_train = to_categorical(y_train, 10)
+    elif dataset == "fashion_mnist":
+        (x_train, y_train), (x_test, y_test) = keras.datasets.fashion_mnist.load_data()
+        x_train = x_train.astype('float32') / 255.0
+        x_test = x_test.astype('float32') / 255.0
+        x_train = x_train.reshape((len(x_train), np.prod(x_train.shape[1:])))
+        x_test = x_test.reshape((len(x_test), np.prod(x_test.shape[1:])))
+        y_test = to_categorical(y_test, 10)
+        y_train = to_categorical(y_train, 10)
+    elif dataset == 'cifer10':
+        image_size = 32
+        latent_units_size = 20
+        (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
+        x_train = x_train.astype('float32') / 255.0
+        x_test = x_test.astype('float32') / 255.0
+        y_test = to_categorical(y_test, 10)
+        y_train = to_categorical(y_train, 10)
+    return (x_train, y_train), (x_test, y_test)
+
+def split_dataset(x_train, y_train):
+    # Unlabled
+    x_train_D1 = x_train[0 : len(x_train) - len(x_train) // 20]
+    y_train_D1 = None
+    # Labled
+    x_train_D2 = x_train[len(x_train) - len(x_train) // 20 :]
+    y_train_D2 = y_train[len(y_train) - len(y_train) // 20 :]
+    return (x_train_D1, y_train_D1), (x_train_D2, y_train_D2)
